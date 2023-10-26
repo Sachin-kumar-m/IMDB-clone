e017bdb08d7667aaf4ad00556ba4cfbbf6c95a4f
sachin.kumar <sachin.kumar.maheshwarappa@gmail.com>
2023-10-26 23:10:40 +0530

    added react-router lib

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './component/Header';
+import Banner from './component/Banner';
+import Movies from './component/Movie';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" />{<Header />}
+        </Routes>
+
+        <Banner />
+        <Movies />
+      </BrowserRouter>
     </div>
   );
 }
