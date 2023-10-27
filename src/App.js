cdfa9853226bfc74ecd6b6f498d6868f64ff55dc
sachin.kumar <sachin.kumar.maheshwarappa@gmail.com>
2023-10-27 13:45:44 +0530

    base components added with few static images

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import './App.css';
 import Header from './component/Header';
 import Banner from './component/Banner';
 import Movies from './component/Movie';
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header/>
         <Routes>
-          <Route path="/" />{<Header />}
+          <Route path="/" element={
+            <>
+              <Banner />
+              <Movies />
+            </>
+          }
+          />
+          <Route path='/watchlist' element={
+            <div>watchlist component</div>
+          }/>
         </Routes>
-
-        <Banner />
-        <Movies />
       </BrowserRouter>
     </div>
   );
