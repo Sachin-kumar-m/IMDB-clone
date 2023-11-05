18e47213aaa4cfadb8e7c4edfb31af885b53528f
sachin.kumar <sachin.kumar.maheshwarappa@gmail.com>
2023-11-05 15:10:09 +0530

    fetching data from API and displaying the data

--- src/component/Movie.js
+++ src/component/Movie.js
@@ -1,41 +1,43 @@
-import { useState } from "react";
-import { json } from "react-router-dom";
-import {url,options} from "../constant.js"
+import { useState, useEffect } from "react";
+// Consts
+import { url, options, BASE_URL } from "../constant.js"
 
 function Movies() {
 
   const [movies, setMovies] = useState([])
-  async function getMovies() {
-
+  const getMovies = () => {
     fetch(url, options)
       .then(res => res.json())
-      .then(json => console.log(json))
+      .then((json)=>setMovies(json.results))
       .catch(err => console.error('error:' + err));
-      return json
   };
-  const updateMovies = ()=>{
-    setMovies(getMovies())
-  }
-updateMovies()
 
- 
-    return (
-      <div>
-        <div className="text-2xl my-8 font-bold text-center underline">
-          Trending Movies
-        </div>
-        <div className="mx-[20px] mb-[12px] flex space-x-8">
-          <div
-            className="w-[160px] h-[30vh] bg-cover rounded-xl m-4 md:h-[40vh] md:w-[180px] hover:scale-110 duration-300"
-            style={{
-              backgroundImage:
-                "url(https://m.media-amazon.com/images/S/pv-target-images/703c93b8ce2fc77bd7bbed52364161a25f1dc078efab6f5c9fbb2b82d042f89e._UR1920,1080_AGaverage_SX1080_FMwebp_.jpg)",
-            }}
-          >
-            <div className="text-xl md:text-3xl bg-gray-900 bg-opacity-60 p-4 text-white w-full">Aspirants</div>
-          </div>
-        </div>
+  useEffect(() => {
+    getMovies()
+  }, [])
+  
+  console.log(movies)
+  return (
+    <div>
+      <div className="text-2xl my-8 font-bold text-center underline">Trending Movies</div>
+      <div className="flex flex-wrap">
+        {movies.map((movi, index) => {
+          return (
+            <div className="mx-[20px] mb-[12px] flex space-x-8" key={index}>
+              <div
+                className="w-[160px] h-[30vh] bg-cover rounded-[1rem] m-4 md:h-[40vh] md:w-[180px] hover:scale-110 duration-300 flex flex-col-reverse	"
+                style={{
+                  backgroundImage:
+                    `url(${BASE_URL}/${movi.poster_path})`
+                }}
+              >
+                <div className="text-xl md:text-3xl bg-gray-900 bg-opacity-60 p-4 text-white w-full rounded-b-[1rem]">{movi.title||movi.name}</div>
+              </div>
+            </div>
+          )
+        })}
       </div>
+    </div>
   );
 }
 
