bd64dc84ba8165e01e9c25a6a84b20765985be94
sachin.kumar <sachin.kumar.maheshwarappa@gmail.com>
2023-11-17 15:35:24 +0530

    page not found component

---
+++ src/component/NotFound.js
@@ -0,0 +1,10 @@
+
+function PageNotFound() {
+    return (
+        <>
+            <div className="h-[100%] w-[100%] absolute flex items-center justify-center text-3xl">404 Page not Found</div>
+        </>
+    )
+}
+
+export default PageNotFound
